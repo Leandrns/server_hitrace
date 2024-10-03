df273041043dfb216b6b43fc07b5478b4901c199
Leandro Souza <leandro2005souza@gmail.com>
2024-10-03 00:20:31 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, { cors: { origin: 'https://hitrace-formula-e.vercel.app/' } })
+const io = require('socket.io')(server, { cors: { origin: 'https://hitrace-formula-e.vercel.app' } })
 const PORT = process.env.PORT || 3001;
 
 io.on('connection', socket => {
